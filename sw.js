a8fcd439b21245afc23632176f5201dc888aff9f
DarkTechLC <cliciosa14@gmail.com>
2020-05-02 12:12:10 -0300

    Make PWA

---
+++ sw.js
@@ -0,0 +1,43 @@
+var CACHE_NAME = 'static-v1';
+
+self.addEventListener('install', function (event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/css/index.css',
+        '/js/index.js',
+        '/js/addTask.js',
+        '/js/clearAll.js',
+        '/js/countPendentsTasks.js',
+        '/js/loadTasks.js',
+        '/js/renderTasks.js',
+        '/js/setTime.js',
+      ]);
+    })
+  )
+});
+
+self.addEventListener('activate', function activator(event) {
+  event.waitUntil(
+    caches.keys().then(function (keys) {
+      return Promise.all(keys
+        .filter(function (key) {
+          return key.indexOf(CACHE_NAME) !== 0;
+        })
+        .map(function (key) {
+          return caches.delete(key);
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (cachedResponse) {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
